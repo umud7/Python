ec54ba34d34f2eb68620f4aac1e5e5cecc0ba3c1
umud <umidhg@code.edu.az>
2023-08-08 22:44:03 +0400

    IP ADRESS BULMA

--- Ip Address/IPMap.py
+++ Ip Address/IPMap.py
@@ -1,2 +1,20 @@
 import geocoder
 import folium
+
+g = geocoder.ip("146.70.3.0")
+
+myAddress = g.latlng
+print(myAddress)
+
+my_map1 = folium.Map(location=myAddress,
+                     zoom_start=12)
+
+
+
+folium.CircleMarker(location=myAddress,
+                    radius=50, popup="Yorkshire").add_to(my_map1)
+
+folium.Marker(myAddress,
+              popup="Yorkshire").add_to(my_map1)
+
+my_map1.save("my_map.html ")
